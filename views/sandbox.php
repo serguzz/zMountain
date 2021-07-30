a664db1e74217ce9864c9659914941aa7f950250
Sergiy <serguz@ukr.net>
2021-07-30 13:22:39 +0300

    newsfeed-section to a separate file; blog pagination buttons lessened in mobile version;

--- views/sandbox.php
+++ views/sandbox.php
@@ -293,67 +293,10 @@ include 'layouts/menu.php';
 		</section>
 
 		<!-----------------content-box-4-------------------->
-		<section class="content-box box-4">
-			<div class="zerogrid">
-				<div class="row wrap-box"><!--Start Box-->
-					<div class="header">
-						<h2>Наші Новини</h2>
-						<p class="intro">Що нового за останній тиждень, місяць, рік в світі туризму та водних видів спорту <br>Звіти про походи та поїздки, тренування та сплави.</p>
-					</div>
-					<div class="content">
-						<div class="column sm-1-3">
-							<div class="wrap-col">
-								<div class="box-entry">
-									<div class="box-entry-inner">
-										<img src="template/images/2.jpg" class="img-responsive"/>
-										<div class="entry-details">
-											<div class="entry-des">
-												<span><a href="#">08th Apr 2017</a></span>
-												<h3><a href="#">Make Ahead Super Green Vegan Quinoa Sandwich.</a></h3>
-												<p>Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum.</p>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="column sm-1-3">
-							<div class="wrap-col">
-								<div class="box-entry">
-									<div class="box-entry-inner">
-										<img src="template/images/3.jpg" class="img-responsive"/>
-										<div class="entry-details">
-											<div class="entry-des">
-												<span><a href="#">08th Apr 2017</a></span>
-												<h3><a href="#">Make Ahead Super Green Vegan Quinoa Sandwich.</a></h3>
-												<p>Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum.</p>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="column sm-1-3">
-							<div class="wrap-col">
-								<div class="box-entry">
-									<div class="box-entry-inner">
-										<img src="template/images/4.jpg" class="img-responsive"/>
-										<div class="entry-details">
-											<div class="entry-des">
-												<span><a href="#">08th Apr 2017</a></span>
-												<h3><a href="#">Make Ahead Super Green Vegan Quinoa Sandwich.</a></h3>
-												<p>Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum.</p>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-
+		<?php
+		 	include "layouts/newsfeed-section.php"?>
+		?>
+	
 		<!-----------------content-box-4.2 - NEW!! made by Serguz-------------------->
 		<section class="content-box box-4">
 			<div class="zerogrid">
